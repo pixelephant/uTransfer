163aa55eb256d55fc4a17c7532bfc73d5e9cbcd6
Balázs Antal <balazs.antal@pixelephant.hu>
2012-03-07 13:12:36 +0100

    Kimaradt
    Signed-off-by: Balázs Antal <balazs.antal@pixelephant.hu>

--- app/models/text_block.rb
+++ app/models/text_block.rb
@@ -5,6 +5,10 @@ class TextBlock < ActiveRecord::Base
 	has_many :text_block_translation
 	accepts_nested_attributes_for :text_block_translation
 
+	has_many :media
+
+	default_scope :order => 'position ASC'
+
 	def default_for_enum
     I18n.available_locales
   end
