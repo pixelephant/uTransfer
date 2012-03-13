17d2adad7d30f16c2d70503252f7114b7fc7ac45
Balázs Antal <balazs.antal@pixelephant.hu>
2012-03-13 15:35:07 +0100

    dreamhost
    Signed-off-by: Balázs Antal <balazs.antal@pixelephant.hu>

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :scm, :git
 set :scm_username, 'pixelephant'
 set :scm_password, 'pix3l3phant'
 
-set :default_environment, { 'PATH' => "'/home/pixelephant/local/bin:/home/pixelephant/.gems/bin:/usr/lib/ruby/gems/1.8/bin/:/usr/local/bin:/usr/bin:/bin:/usr/bin/X11:/usr/games'" }
+set :default_environment, { 'PATH' => "'/home/pixelephant/local/bin:/home/pixelephant/.gems/bin:/usr/lib/ruby/gems/1.8/bin/:/usr/local/bin:/usr/bin:/bin:/usr/bin/X11:/usr/games:/home/pixelephant/.gems/bin/rake'" }
 
 role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
