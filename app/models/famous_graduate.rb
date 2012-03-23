d82e26b9dba1970da0ab294076f838a530d09cfd
Balázs Antal <balazs.antal@pixelephant.hu>
2012-03-23 12:51:45 +0100

    famous graduate uploader
    Signed-off-by: Balázs Antal <balazs.antal@pixelephant.hu>

--- app/models/famous_graduate.rb
+++ app/models/famous_graduate.rb
@@ -5,6 +5,8 @@ class FamousGraduate < ActiveRecord::Base
 	has_many :famous_graduate_translation
 	accepts_nested_attributes_for :famous_graduate_translation
 
+	mount_uploader :image_file, FamousPersonUploader
+
 	validates :name, :title, :university_id, :presence => true
 
 	belongs_to :university
