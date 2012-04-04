d01e738398d6015c24505dfa91864b022e8d4ac1
Balázs Antal <balazs.antal@pixelephant.hu>
2012-04-04 16:31:15 +0200

    University/show -> faculty-k szétválasztva

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+	def int_to_eng(i)
+		case i
+			when 1
+				return "one"
+			when 2
+				return "two"
+			when 3
+				return "three"
+			when 4
+				return "four"
+			when 5
+				return "five"
+			when 6
+				return "six"
+		end
+	end
+
 end
