dcba2f14aec9aa645985f299098cf11a76187960
Balázs Antal <balazs.antal@pixelephant.hu>
2012-04-05 15:11:38 +0200

    ckeditor config.js -> nbsp; <p></p> ki
    Signed-off-by: Balázs Antal <balazs.antal@pixelephant.hu>

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -8,7 +8,11 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	
+
+  config.fillEmptyBlocks = false;
+  config.ignoreEmptyParagraph = true;
+  config.enterMode = CKEDITOR.ENTER_BR;
+
   /* Filebrowser routes */
   // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
   config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
@@ -18,7 +22,7 @@ CKEDITOR.editorConfig = function( config )
 
   // The location of a script that handles file uploads in the Flash dialog.
   config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
-  
+
   // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Link tab of Image dialog.
   config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
 
@@ -27,30 +31,30 @@ CKEDITOR.editorConfig = function( config )
 
   // The location of a script that handles file uploads in the Image dialog.
   config.filebrowserImageUploadUrl = "/ckeditor/pictures";
-  
+
   // The location of a script that handles file uploads.
   config.filebrowserUploadUrl = "/ckeditor/attachment_files";
-  
+
   // Rails CSRF token
   config.filebrowserParams = function(){
     var csrf_token = jQuery('meta[name=csrf-token]').attr('content'),
         csrf_param = jQuery('meta[name=csrf-param]').attr('content'),
         params = new Object();
-    
+
     if (csrf_param !== undefined && csrf_token !== undefined) {
       params[csrf_param] = csrf_token;
     }
-    
+
     return params;
   };
-  
+
   /* Extra plugins */
   // works only with en, ru, uk locales
   config.extraPlugins = "embed,attachment";
-  
+
   /* Toolbars */
   config.toolbar = 'Easy';
-  
+
   config.toolbar_Easy =
     [
         ['Source','-','Preview'],
