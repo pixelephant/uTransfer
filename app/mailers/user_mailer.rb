d7f26fd0f84767e3220968154614085a705525aa
Balázs Antal <balazs.antal@pixelephant.hu>
2012-04-23 15:50:39 +0200

    javítások
    Signed-off-by: Balázs Antal <balazs.antal@pixelephant.hu>

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,18 +1,18 @@
 class UserMailer < ActionMailer::Base
-  default :from => "test@utransferhungary.com"
+  default :from => "info@utransferhungary.com"
 
 	def contact_confirmation(contact)
-	  recipients 		"test@utransferhungary.com"
-	  from 					contact.email
-	  subject 			"Website contact form [#{contact.why}]"
+	  recipients 		"info@utransferhungary.com"
+	  from 					"info@utransferhungary.com"
+	  subject 			"Website contact form - #{contact.why}"
 	  #body        :contact => contact
-	  body 					contact.message
+	  body 					"E-mail: " + contact.email + "  Message: " + contact.message
 	end
 
 	def offer_confirmation(contact)
 		@contact = contact
-	  recipients = "test@utransferhungary.com"
-	  from = contact.email
+	  recipients = "offer@utransferhungary.com"
+	  from = "offer@utransferhungary.com"
 	  subject = "Personal offer from website"
 	  #body        :contact => contact
 	  mail(:to => recipients, :from => from, :subject => subject)
@@ -20,8 +20,8 @@ class UserMailer < ActionMailer::Base
 
 	def offer_additional(contact)
 		@contact = contact
-	  recipients = "test@utransferhungary.com"
-	  from = contact.email
+	  recipients = "offer@utransferhungary.com"
+	  from = "offer@utransferhungary.com"
 	  subject = "Additional informations"
 	  #body        :contact => contact
 	  mail(:to => recipients, :from => from, :subject => subject)
